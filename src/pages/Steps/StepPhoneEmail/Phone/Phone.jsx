e661d0bb5acf616573ef8f93315985d2b99cbd2f
wempyv <wempyv@gmail.com>
2022-03-19 23:21:05 +0700

    📦

--- src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../../../../components/shared/Card/Card";
 import Button from "../../../../components/shared/Button/Button";
 import TextInput from "../../../../components/shared/TextInput/TextInput";
+import styles from "../StepPhoneEmail.module.css";
 
 const Phone = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -9,10 +10,16 @@ const Phone = () => {
     <Card title="Enter your phone number" icon="phone">
       <TextInput
         value={phoneNumber}
-        onChange={() => setPhoneNumber(e.target.value)}
+        onChange={(e) => setPhoneNumber(e.target.value)}
       />
       <div>
-        <Button text="Next" />
+        <div className={styles.actionButtonWrap}>
+          <Button text="Next" />
+        </div>
+        <p className={styles.bottomParagraph}>
+          By entering your number, you're agreeing to our Terms of Service and
+          Privacy Policy. Thanks !
+        </p>
       </div>
     </Card>
   );
